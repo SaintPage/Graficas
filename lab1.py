6f8973d71cb5fd705fd1fcf80610c1f767110a30
angel <mer23661@uvg.edu.gt>
2025-07-09 22:09:54 -0600

    Ultimos arreglos

--- lab1.py
+++ lab1.py
@@ -6,11 +6,11 @@ from BMP_Writer import GenerateBMP
 
 class ScanlineRenderer(Renderer):
     def glFillPolygon(self, poly, color=None):
-        # 1) Fijar el color de relleno en el renderer
+        #Fijar el color de relleno en el renderer
         if color is not None:
             self.glColor(*color)
 
-        # 2) Scanline como antes
+        
         ys = [p[1] for p in poly]
         y_min, y_max = min(ys), max(ys)
 
@@ -57,7 +57,7 @@ poly3 = [(377,249),(411,197),(436,249)]
 poly4 = [(413,177),(448,159),(502, 88),(553, 53),(535, 36),(676, 37),
          (660, 52),(750,145),(761,179),(672,192),(659,214),(615,214),
          (632,230),(580,230),(597,215),(552,214),(517,144),(466,180)]  # tetera
-poly5 = [(682,175),(708,120),(735,148),(739,170)]  # agarre
+poly5 = [(682,175),(708,120),(735,148),(739,170)]  #hueco
 
 # colores
 YELLOW = (1.0, 1.0, 0.0)
@@ -76,16 +76,19 @@ def main():
 
         rend.glClear()
 
-        # 1) Estrella → amarillo
+        #Poligono1
         rend.glFillPolygon(poly1, YELLOW)
 
-        # 2) Cuadrado y triángulo → blanco
+        #Poligono2
         rend.glFillPolygon(poly2, WHITE)
-        rend.glFillPolygon(poly3, WHITE)
 
-        # 3) Tetera completa → blanco
+        #Poligono3
+        rend.glFillPolygon(poly3, WHITE)
+        
+        #Poligono4
         rend.glFillPolygon(poly4, WHITE)
-        # 4) Hueco (agarre) → negro, recupera el fondo
+        
+        #Poligono5
         rend.glFillPolygon(poly5, BLACK)
 
         pygame.display.flip()
